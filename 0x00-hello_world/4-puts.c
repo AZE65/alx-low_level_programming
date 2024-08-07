fa5e1b3b77fc916c7230cb6c9cd3600360bab984
Ahmed Eid <ahmedeidabdeiazema@gmail.com>
2024-08-07 03:23:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
-#include <stdio.h> 
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
+#include <stdio.h>
+
 /**
- * main - A program that prints a line with functio
- * n 
- * Return: 0 (Saccess)
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-		return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }	
